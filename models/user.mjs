4fd69d616a63d01122fdb4279ab5a2e507fcd76a
Katie Nink Smith <katiej.smith@outlook.com>
2025-03-03 09:05:38 -0700

    BE03_2 user model complete

--- models/user.mjs
+++ models/user.mjs
@@ -1,7 +1,44 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({});
+const userSchema = new Schema([
+  {
+    user_name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      trim: true,
+    },
+    // for furture integration no one had confirmed if this should be added now
+    timezone: {
+      type: String,
+    },
+    age: {
+      type: String,
+      enum: [over, under],
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transfrom: function (doc, ret) {
+        delete ret.password;
+        return ret;
+      },
+    },
+  },
+]);
 
 const Users = mongoose.model("user", userSchema);
 
