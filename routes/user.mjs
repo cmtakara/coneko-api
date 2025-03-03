667ddacf71bd92abc5b39ea7bb8b15376fc08355
Katie Nink Smith <katiej.smith@outlook.com>
2025-03-03 10:56:47 -0700

    BE03_6 stopped tracking of user files

--- routes/user.mjs
+++
@@ -1,8 +0,0 @@
-// import express from "express";
-// const router = express.Router();
-// import userController from "../controller/user.mjs";
-
-// // ! seed route to be removed later
-// router.post("/seed", userController.seed);
-
-// export default router;
