f063af83cabab32c686ea85367e4bbe7c4a1020c
Hema-mv <mvshema@gmail.com>
2025-03-03 14:23:04 -0600

    Create apikeyvalidator.js
    APIKey Validation

---
+++ middlewares/apikeyvalidator.js
@@ -0,0 +1,24 @@
+
+
+const apiKey = process.env.API_KEY;
+
+
+const validateApiKey = async (req, res, next) => {
+    const providedApiKey = req.headers['x-api-key'];
+    if (providedApiKey) {
+        try {
+            const key = await apiKey.findOne({ key: providedApiKey });
+            if (key) {
+                next();
+            } else {
+                res.status(403).send('Forbidden');
+            }
+        } catch (err) {
+            res.status(500).send('Server error');
+        }
+    } else {
+        res.status(400).send('API key is required');
+    }
+};
+
+export default validateApiKey;
